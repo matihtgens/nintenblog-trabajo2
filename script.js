2df7bff0a82ef89857ecd4fe55babbd632f6bc93
Matias Henriquez <matias.henriquez@genstudents.org>
2024-09-13 17:08:23 -0300

    Cambios generales, optimizacion, orden, implementacion de bootstrap, etc.

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
+// Script para El formulario
+(() => {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+
+            form.classList.add('was-validated')
+        }, false)
+    })
+})()
+
 function presionarBoton() {
-    let nombre = document.getElementById("fname").value;
-    let apellido = document.getElementById("lname").value;
+    let nombre = document.getElementById("validationCustom01").value;
+    let apellido = document.getElementById("validationCustom02").value;
     let mensaje = document.getElementById("message").value;
 
     if (nombre == '' || apellido == '' || mensaje == '') {
@@ -9,8 +29,8 @@ function presionarBoton() {
     } else {
         console.log("Mensaje enviado: " + mensaje);
         alert("Gracias por tu mensaje: " + nombre + " " + apellido + "! Te responderemos en breve. Tu mensaje aparece en la consola.");
-        document.getElementById('fname').value = '';
-        document.getElementById('lname').value = '';
+        document.getElementById('validationCustom01').value = '';
+        document.getElementById('validationCustom02').value = '';
         document.getElementById('message').value = '';
         return;
     }
